244c806c4080d697af1eedf402b03c4c92c9644a
Chandr1330 <105033250+Chandr1330@users.noreply.github.com>
2022-05-10 17:39:22 +0530

    Add files via upload

---
+++ src/test/java/BootStrapAssignment.java
@@ -0,0 +1,45 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.Test;
+
+public class BootStrapAssignment {
+WebDriver driver;
+
+	@Test
+	
+	public void test() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "D:\\SoftwareCenterApplications\\ChromeDriver 100\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.jquery-az.com/boots/demo.php?ex=63.0_2");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("/html/body/div[3]/table/tbody/tr[2]/td[3]/div/span/div/button/span")).click();
+		
+		List<WebElement> list = driver.findElements(By.xpath("/html/body/div[3]/table/tbody/tr[2]/td[3]/div/span/div/ul"));
+		System.out.println(list.size());
+		
+		for(int i = 0;i<list.size();i++) {
+			System.out.println(list.get(i).getText());
+			Thread.sleep(5000);
+			
+			
+			if(list.get(i).getText().contains("Angular")) {
+				list.get(i).click();
+				break;
+			}
+			
+		}
+		driver.close();
+		
+			
+		
+		
+	}
+
+}
